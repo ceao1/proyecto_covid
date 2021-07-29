4fb0b7d4f2a8806362a9f864c4736bc55f40e67e
Carlos Anzola <carloseduardoanzola93@gmail.com>
2021-07-28 21:47:22 -0500

    cambio de json a csv

--- transform.py
+++ transform.py
@@ -9,4 +9,4 @@ archivos.sort()
 for archivo in archivos:
     temp_df = pd.read_json(archivo)
     temp_df_agrupado = temp_df.groupby(['fecha_reporte_web','departamento_nom','ciudad_municipio_nom'])['id_de_caso'].count().reset_index()
-    temp_df_agrupado.to_json('../resumen_' + archivo)
+    temp_df_agrupado.to_csv('../resumen_' + archivo.replace('.json', '.csv'), index=False)
