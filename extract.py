591fadb2d429f4c322ee3b9a7d68e3a9039b58af
Carlos Anzola <carloseduardoanzola93@gmail.com>
2021-07-29 11:41:53 -0500

    agrego comentarios para claridad de codigo

--- extract.py
+++ extract.py
@@ -5,12 +5,12 @@ import json
 from datetime import datetime
 from datetime import timedelta
 
-hoy = datetime.today()
-otro_dia = hoy + timedelta(days=-5)
+ayer = datetime.today() + timedelta(days=-1)
+otro_dia = ayer + timedelta(days=-1)
 
 
 #convierto la fecha de hoy en el formato que lo acepta la consulta de api
-fecha = str(hoy.day) + '/' + str(hoy.month) + '/' + str(hoy.year) + ' 0:00:00'
+fecha = str(ayer.day) + '/' + str(ayer.month) + '/' + str(ayer.year) + ' 0:00:00'
 #fecha = str(otro_dia.day) + '/' + str(otro_dia.month) + '/' + str(otro_dia.year) + ' 0:00:00'
 
 
@@ -19,13 +19,14 @@ resultado = requests.get("https://www.datos.gov.co/resource/gt2j-8ykr.json?$limi
 
 
 #se crea una variable con el nombre con el que se guardaran los datos, en formato json
-nombre_archivo = str(hoy.date()).replace('-', '') + '.json'
+nombre_archivo = str(ayer.date()).replace('-', '') + '.json'
 #nombre_archivo = str(otro_dia.date()).replace('-', '') + '.json'
 
 
 #se crea la ruta para guardar el archivo
-ruta_archivo = './data/' + nombre_archivo
+ruta_archivo = './data/data_cruda/' + nombre_archivo
 
+#se crea el archivo con el nombre asociado al día descargado
 with open(ruta_archivo, 'w') as fp:
     json.dump(resultado.json(), fp)
 
--- transform.py
+++ transform.py
@@ -2,10 +2,12 @@ import json
 import pandas as pd
 import os
 
+#nos situamos en la direccion de donde queremos llamar los archivos crudos
 os.chdir('./data/data_cruda')
 archivos = os.listdir()
 archivos.sort()
 
+#generamos un bucle por todos los archivos para leerlos, agrupar la información a nivel de municipio contando los casos nuevos. Posteriormente se guarda como csv en el directorio padre del actual.
 for archivo in archivos:
     temp_df = pd.read_json(archivo)
     temp_df_agrupado = temp_df.groupby(['fecha_reporte_web','departamento_nom','ciudad_municipio_nom'])['id_de_caso'].count().reset_index()
