314342beeb138383b55a39fa55b519890e6fdefb
Carlos Anzola <carloseduardoanzola93@gmail.com>
2021-07-28 21:32:11 -0500

    creacion de transform

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # proyecto_covid
+
+Este proyecto tiene por objetivo practicar el llamado de información pública del Covid proveniente del Instituto Nacional de Salud de Colombia, hacer algunas agrupaciones y generar un reporte básico.
---
+++ transform.py
@@ -0,0 +1,12 @@
+import json
+import pandas as pd
+import os
+
+os.chdir('./data/data_cruda')
+archivos = os.listdir()
+archivos.sort()
+
+for archivo in archivos:
+    temp_df = pd.read_json(archivo)
+    temp_df_agrupado = temp_df.groupby(['fecha_reporte_web','departamento_nom','ciudad_municipio_nom'])['id_de_caso'].count().reset_index()
+    temp_df_agrupado.to_json('../resumen_' + archivo)
